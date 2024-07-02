aee48543b91501b23612ce6453f3686f9a8f040b
Jitendra Sachwani(DeskPC) <sachwani.jitendra@gmail.com>
2024-07-02 15:06:01 +0530

    reafctoring dotfiles

--- packages/custom_packages.nix
+++ packages/custom_packages.nix
@@ -3,10 +3,10 @@
 {
   home.packages = with pkgs;[
     # Add packages specific to current branch here
-    sonarr
-    radarr
-    lidarr
-    jackett
+    # sonarr
+    # radarr
+    # lidarr
+    # jackett
     # transmission
   ];
 }
