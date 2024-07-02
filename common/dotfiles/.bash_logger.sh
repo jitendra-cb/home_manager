e62224bde4454252cc894a7d0622090f18d14289
Jitendra Sachwani <jitendra.sachwani@coinbase.com>
2024-07-02 17:07:29 +0530

    refactor

--- common/dotfiles/.bash_logger.sh
+++ common/dotfiles/.bash_logger.sh
@@ -2,8 +2,6 @@
 
 BOLD_BLUE='\033[0;1;34m'
 BOLD_GREEN='\033[0;1;32m'
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
 BOLD_YELLOW='\033[0;1;33m'
 BOLD_RED='\033[0;1;31m'
 BOLD_PURPLE='\033[0;1;35m'
@@ -29,22 +27,3 @@ logVerboseWarning () {
     echo -e "${BOLD_YELLOW}$1${RESET}"
   fi
 }
-=======
-=======
->>>>>>> Stashed changes
-BOLD_RED='\033[0;1;31m'
-BOLD_YELLOW='\033[0;1;33m'
-
-RESET='\033[0m'
-
-err() { echo -e "${BOLD_RED}$1${RESET}"; }
-
-warn() { echo -e "${BOLD_YELLOW}$1${RESET}"; }
-
-info() { echo -e "${BOLD_BLUE}$1${RESET}"; }
-
-success() { echo -e "${BOLD_GREEN}$1${RESET}"; }
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
