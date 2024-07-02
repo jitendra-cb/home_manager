d7c08732162aad5e97b318586c4be25f8f5801ee
Jitendra Sachwani(DeskPC) <sachwani.jitendra@gmail.com>
2024-07-02 16:49:39 +0530

    restructure common and custom folders

--- common/packages/general.nix
+++ common/packages/general.nix
@@ -2,14 +2,17 @@
 
 {
   home.packages = with pkgs;[
+    (writeShellScriptBin "hello" ''
+        echo "Hello, ${config.home.username}!"
+    '')
+
+    asciiquarium
     awscli2
-    duf
+    delta
     fx
     fzf
     less
     nixpkgs-fmt
-    ncdu
-    neofetch
     ripgrep-all
     (writeShellScriptBin "rga-fzf" ''
       RG_PREFIX="rga --files-with-matches"
@@ -24,8 +27,22 @@
       echo "opening $file" &&
       xdg-open "$file"
     '')
+    tldr
     tree
-    unzip
-    zip
+    # unzip
+    # vscode
+    # vlc
+    # zip
+
+    # TODO: try
+    # glow
+    # gum
+    # helix
+
+    # TODO: categorise
+    # iterm2
+    # mosh
+    # ngrok
+    # slack
   ];
 }
--- dotfiles.nix
+++ dotfiles.nix
@@ -2,9 +2,16 @@
 
 {
   home.file = {
+    
+    # Common Dotfiles  
     ".aliases".source = ./dotfiles/.aliases;
     ".bash_logger.sh".source = ./dotfiles/.bash_logger.sh;
+    ".gemrc".source = ./dotfiles/.gemrc;
     ".vimrc".source = ./dotfiles/.vimrc;
     ".zshrc".source = ./dotfiles/.zshrc;
+
+    # Custom Dotfiles
+    ".custom_aliases".source = ./custom/dotfiles/.aliases;
+    
   };
 }
