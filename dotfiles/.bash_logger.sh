e4e124d47b50bdaecee1177076c7753a25fa616d
Jitendra Sachwani(DeskPC) <sachwani.jitendra@gmail.com>
2024-07-02 14:43:12 +0530

    added home manager setup in updated ru.sh

--- dotfiles/.bash_logger.sh
+++ dotfiles/.bash_logger.sh
@@ -8,16 +8,22 @@ BOLD_PURPLE='\033[0;1;35m'
 
 RESET='\033[0m'
 
-logError() { echo -e "${BOLD_RED}$1${RESET}" ; }
-
-logWarning() { echo -e "${BOLD_YELLOW}$1${RESET}" ; }
-
 logInfo() { echo -e "${BOLD_BLUE}$1${RESET}" ; }
 
 logSuccess() { echo -e "${BOLD_GREEN}$1${RESET}" ; }
 
-logVerbose () {
-    if [[ $_V -eq 1 ]]; then
-        echo -e "${BOLD_PURPLE}$1${RESET}"
-    fi
+logWarning() { echo -e "${BOLD_YELLOW}$1${RESET}" ; }
+
+logError() { echo -e "${BOLD_RED}$1${RESET}" ; }
+
+logVerboseInfo () {
+  if [[ $_V -eq 1 ]]; then
+    echo -e "${BOLD_PURPLE}$1${RESET}"
+  fi
+}
+
+logVerboseWarning () {
+  if [[ $_V -eq 1 ]]; then
+    echo -e "${BOLD_YELLOW}$1${RESET}"
+  fi
 }
--- run.sh
+++ run.sh
@@ -12,55 +12,64 @@ done
 ABS_SCRIPT_DIR="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
 source $ABS_SCRIPT_DIR/dotfiles/.bash_logger.sh
-logVerbose "Sourced: $ABS_SCRIPT_DIR/dotfiles/.bash_logger.sh"
+logVerboseInfo "Sourced: $ABS_SCRIPT_DIR/dotfiles/.bash_logger.sh"
 set -e
 
 get_keys() {
   logInfo "[+] Setting up Keys"
   
-  mkdir -p "$HOME/.ssh" && logVerbose "-> Ensured the .ssh folder is present"
-  chmod 700 "$HOME/.ssh" && logVerbose '--> Set permissions for the .ssh folder'
-  # TODO: GET SSH Public Key && logVerbose '--> Fetched .ssh/id_rsa.pub'
-  chmod 644 "$HOME/.ssh/id_rsa.pub" && logVerbose '--> Set permissions for .ssh/id_rsa.pub'
-  # TODO: GET SSH Private Key && logVerbose '--> Fetched .ssh/id_rsa'
-  chmod 600 "$HOME/.ssh/id_rsa" && logVerbose '--> Set permissions for .ssh/id_rsa'
+  mkdir -p "$HOME/.ssh" && logVerboseInfo "--> Ensured the .ssh folder is present"
+  chmod 700 "$HOME/.ssh" && logVerboseInfo '----> Set permissions for the .ssh folder'
+  # TODO: GET SSH Public Key && logVerboseInfo '--> Fetched .ssh/id_rsa.pub'
+  chmod 644 "$HOME/.ssh/id_rsa.pub" && logVerboseInfo '----> Set permissions for .ssh/id_rsa.pub'
+  # TODO: GET SSH Private Key && logVerboseInfo '--> Fetched .ssh/id_rsa'
+  chmod 600 "$HOME/.ssh/id_rsa" && logVerboseInfo '----> Set permissions for .ssh/id_rsa'
   
-  mkdir -p "$HOME/.gnupg" && logVerbose "-> Ensured .gnupg folder is present"
-  chmod 700 "$HOME/.gnupg" && logVerbose '--> Set permissions for the .gnupg folder'
-  # TODO: GET SSH Private Key && logVerbose '--> Imported GPG keys'
+  mkdir -p "$HOME/.gnupg" && logVerboseInfo "--> Ensured .gnupg folder is present"
+  chmod 700 "$HOME/.gnupg" && logVerboseInfo '----> Set permissions for the .gnupg folder'
+  # TODO: GET SSH Private Key && logVerboseInfo '--> Imported GPG keys'
   
   logSuccess "-> Keys Set Up!"
 }
 
 get_keys
 
+setup_home_manager() {
+  logInfo "[+] Setting up Home Manager"
+  
+  if !(nix --version); then
+    logVerboseInfo "--> Installing Nix"
+    sh <(curl -L https://nixos.org/nix/install) --daemon
+  else
+    logVerboseWarning "--> Nix already installed!"
+  fi
+  
+  
+  if ! [ -f ~/.config/nix/nix.conf ]; then
+    mkdir -p ~/.config/nix/ && logVerboseInfo "--> Ensured the .config/nix/ folder is present"
+    cp $ABS_SCRIPT_DIR/dotfiles/nix.conf ~/.config/nix/nix.conf && logVerboseInfo '--> Added nix.conf'
+  else
+    logVerboseWarning "--> Nix config already present!"
+  fi
+  
+  
+  if !(nix-channel --list | grep home-manager &> /dev/null); then
+    nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
+    nix-channel --update && logVerboseInfo "--> Added Home manager nix-channel"
+  else
+    logVerboseWarning "--> Home manager nix-channel already added!"
+  fi
+  
+  if !(home-manager --version &> /dev/null); then
+    logVerboseInfo "--> Installing Home Manager"
+    nix-shell '<home-manager>' -A install
+  else
+    logVerboseWarning "--> Home manage already installed!"
+  fi
+  
+  logVerboseInfo "--> Switching to Home Manager"
+  home-manager switch --flake $ABS_SCRIPT_DIR --show-trace -b bkp
+  logSuccess "-> Home Manager Set Up!"
+}
 
-# logInfo "[+] Setting up Home Manager"
-# if !(nix --version); then
-#   sh <(curl -L https://nixos.org/nix/install) --daemon
-# else
-#   logWarning "--> Nix already installed"
-# fi
-
-# if ! [ -f ~/.config/nix/nix.conf ]; then
-#   mkdir -p ~/.config/nix/
-#   cp $ABS_SCRIPT_DIR/dotfiles/nix.conf ~/.config/nix/nix.conf
-# else
-#   logWarning "--> Nix config already present"
-# fi
-
-# if !(nix-channel --list | grep home-manager &> /dev/null); then
-#   nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
-#   nix-channel --update
-# else
-#   logWarning "-> Home manage channel already exists!"
-# fi
-
-# if !(home-manager --version &> /dev/null); then
-#   nix-shell '<home-manager>' -A install
-# else
-#   logWarning "-> Home manage already installed!"
-# fi
-
-# home-manager switch --flake $ABS_SCRIPT_DIR --show-trace -b bkp
-# logSuccess "-> Home Manager Set Up!"
+setup_home_manager
