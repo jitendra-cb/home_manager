e7600e37c96d8916e48557f91e9687d18f4e8f78
Jitendra Sachwani(DeskPC) <sachwani.jitendra@gmail.com>
2024-07-02 17:12:17 +0530

    restructure common and custom folders

--- dotfiles.nix
+++ dotfiles.nix
@@ -4,11 +4,11 @@
   home.file = {
     
     # Common Dotfiles  
-    ".aliases".source = ./dotfiles/.aliases;
-    ".bash_logger.sh".source = ./dotfiles/.bash_logger.sh;
-    ".gemrc".source = ./dotfiles/.gemrc;
-    ".vimrc".source = ./dotfiles/.vimrc;
-    ".zshrc".source = ./dotfiles/.zshrc;
+    ".aliases".source = ./common/dotfiles/.aliases;
+    ".bash_logger.sh".source = ./common/dotfiles/.bash_logger.sh;
+    ".gemrc".source = ./common/dotfiles/.gemrc;
+    ".vimrc".source = ./common/dotfiles/.vimrc;
+    ".zshrc".source = ./common/dotfiles/.zshrc;
 
     # Custom Dotfiles
     ".custom_aliases".source = ./custom/dotfiles/.aliases;
